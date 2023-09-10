6815ce26bb9ef2a2bbce29e0f0b8ee87215941cf
Rainer Holzner <rholzner@web.de>
2023-09-10 11:04:32 +0200

    Bugfix for box number calculation, other improvements
    Fixed error in cell number to box number calculation - cell_to_box().
    Initialize boxes in a loop.
    Use unsigned data type instead of size_t where appropriate.
    Fixed typo in solver manpage.

--- solver.c
+++ solver.c
@@ -22,21 +22,20 @@ typedef struct _cell
 static Cell cells[9*9];
 static Cell *rows[9][9];
 static Cell *cols[9][9];
-static Cell *boxes[9][9] = {
-	{ CL( 0), CL( 1), CL( 2), CL( 9), CL(10), CL(11), CL(18), CL(19), CL(20) },
-	{ CL( 3), CL( 4), CL( 5), CL(12), CL(13), CL(14), CL(21), CL(22), CL(23) },
-	{ CL( 6), CL( 7), CL( 8), CL(15), CL(16), CL(17), CL(24), CL(25), CL(26) },
-	{ CL(27), CL(28), CL(29), CL(36), CL(37), CL(38), CL(45), CL(46), CL(47) },
-	{ CL(30), CL(31), CL(32), CL(39), CL(40), CL(41), CL(48), CL(49), CL(50) },
-	{ CL(33), CL(34), CL(35), CL(42), CL(43), CL(44), CL(51), CL(52), CL(53) },
-	{ CL(54), CL(55), CL(56), CL(63), CL(64), CL(65), CL(72), CL(73), CL(74) },
-	{ CL(57), CL(58), CL(59), CL(66), CL(67), CL(68), CL(75), CL(76), CL(77) },
-	{ CL(60), CL(61), CL(62), CL(69), CL(70), CL(71), CL(78), CL(79), CL(80) }
-};
+static Cell *boxes[9][9];
+
+static unsigned cell_to_box(unsigned cell_no)
+{
+	unsigned box_y, box_x;
+
+	box_y = (cell_no/9)/3;
+	box_x = (cell_no%9)/3;
+	return box_y*3+box_x;
+}
 
 static void init(void)
 {
-	size_t i, j;
+	unsigned i, j, box;
 	Cell empty = { .value = 0, .ct = CT_BLANK };
 
 	for (i = 0; i < 9; i++)
@@ -46,6 +45,18 @@ static void init(void)
 			cells[i*9+j] = empty;
 			rows[i][j] = &cells[i*9+j];
 			cols[i][j] = &cells[j*9+i];
+		}
+	}
+
+	for (box = 0; box < 9; box++)
+	{
+		j = 0;
+		for (i = 0; i < 81; i++)
+		{
+			if (cell_to_box(i) == box)
+			{
+				boxes[box][j++] = CL(i);
+			}
 		}
 	}
 }
@@ -76,7 +87,7 @@ static int read_puzzle(void)
 
 static void print_cells(void)
 {
-	size_t x, y;
+	unsigned x, y;
 
 	for (y = 0; y < 9; y++)
 	{
@@ -99,30 +110,24 @@ static void print_cells(void)
 static int check_unique(const Cell *a[])
 {
 	unsigned bits = 0;
+	unsigned mask;
+	unsigned i;
 	const Cell *cl;
 
-	for (size_t i = 0; i < 9; i++)
+	for (i = 0; i < 9; i++)
 	{
 		cl = a[i];
 		if (cl->ct == CT_VALUE || cl->ct == CT_FIXED)
 		{
-			if (bits & (1 << cl->value))
+			mask = 1 << cl->value;
+			if (bits & mask)
 			{
 				return 0;
 			}
-			bits |= (1 << cl->value);
+			bits |= mask;
 		}
 	}
 	return 1;
-}
-
-static int cell_to_box(int cell_no)
-{
-	int box_y, box_x;
-
-	box_y = (cell_no/9)/3;
-	box_x = (cell_no%9)%3;
-	return box_y*3+box_x;
 }
 
 static int check_cell(int cell_no)
@@ -137,7 +142,7 @@ static int check_cell(int cell_no)
  */
 static int check_all(void)
 {
-	int cell_no;
+	unsigned cell_no;
 
 	for (cell_no = 0; cell_no < 81; cell_no++)
 	{
